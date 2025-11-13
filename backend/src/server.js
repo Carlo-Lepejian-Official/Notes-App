6454cf4f6a38f8f1e888963fd33efb26c32c4c59
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-14 08:13:44 +1100

    Add rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRouter from "./routes/notesRoutes.js";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use(express.json());
+app.use(rateLimiter)
 
 app.use("/api/notes", notesRouter);
 
