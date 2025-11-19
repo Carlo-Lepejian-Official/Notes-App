53c1b19d7d23daab791c041f33411ef98522713f
Carlo Lepejian <carlo.lepejian@outlook.com>
2025-11-19 20:38:43 +1100

    Make the website production ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
 
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -8,21 +9,35 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: ["http://localhost:5173"]
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
-
 app.use("/api/notes", notesRouter);
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Listening on: http://localhost:5001/");
-  });
-}).catch((error) => {
-  console.error("Couldn't connect to database: ", error)
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Listening on: http://localhost:5001/");
+    });
+  })
+  .catch((error) => {
+    console.error("Couldn't connect to database: ", error);
+  });
